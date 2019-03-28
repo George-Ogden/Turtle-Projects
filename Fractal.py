bb32b337ff783c05cf082c82c87727ff43814bab
George Ogden <G02287@outlook.com>
2019-03-28 20:15:12 +0000

    New fractal!

---
+++ Fractal.py
@@ -0,0 +1,38 @@
+#Fractals along a line
+import time
+import math
+import turtle
+def line(startx,starty, endx,endy):
+    fullx = endx-startx
+    fully = endy-starty
+    distance = math.sqrt(fully**2+fullx**2)
+    if distance < 6:
+        turtle.penup()
+        turtle.goto(startx,starty)
+        turtle.pendown()
+        turtle.goto(endx,endy)
+        return
+    thirdx = fullx/3
+    thirdy = fully/3
+    Ax = startx+thirdx
+    Ay = starty+thirdy
+    Cx = endx-thirdx
+    Cy = endy-thirdy
+    Bx = Ax-thirdy
+    By = thirdx+Ay
+    Dx = Cx+thirdy
+    Dy = -thirdx+Cy
+    line(startx,starty, Ax,Ay)
+    line(Ax,Ay, Bx,By)
+    line(Ax,Ay, Cx,Cy)
+    line(Cx,Cy, Dx,Dy)
+    line(Cx,Cy, endx,endy)
+
+while True:
+    turtle.ht()
+    turtle.speed(0)
+    turtle.pencolor("aqua")
+    turtle.bgcolor("black")
+    line(-600,0,600,0)
+    time.sleep(10)
+    turtle.reset()
