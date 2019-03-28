81fc12dd29fae82833eb1c7821c1e3ff3473d84f
George-Ogden <38294960+George-Ogden@users.noreply.github.com>
2019-03-28 15:01:10 +0000

    Neatened code

--- Fractals.py
+++ Fractals.py
@@ -1,29 +1,35 @@
-import turtle as tur
-import time, math
+#Fractals along a line
+import time
+import math
+import turtle
 def line(startx,starty, endx,endy):
     fullx = endx-startx
     fully = endy-starty
     distance = math.sqrt(fully**2+fullx**2)
-    if distance < 5:
-        tur.pu()
-        tur.goto(startx,starty)
-        tur.pd()
-        tur.goto(endx,endy)
+    if distance < 6:
+        turtle.penup()
+        turtle.goto(startx,starty)
+        turtle.pendown()
+        turtle.goto(endx,endy)
         return
-    halfx,halfy = fullx/2,fully/2
-    Ax,Ay = halfx+startx,halfy+starty
-    Bx,By = Ax-(1/2*(endy-Ay)),1/2*(endx-Ax)+Ay
+    halfx = fullx/ 2
+    halfy = fully/2
+    Ax = halfx+startx
+    Ay = halfy+starty
+    rotatex = Ax-(1/2*(halfy))
+    rotatey = 1/2*(halfx)+Ay
+    Bx = rotatex
+    By = rotatey
     line(startx,starty, Ax,Ay)
     line(Ax,Ay, Bx,By)
     line(Bx,By, Ax,Ay)
     line(Ax,Ay, endx,endy)
 
-tur.ht()
-tur.speed(0)
-tur.color("aqua")
-tur.bgcolor("black")
-tur.pu()
 while True:
-    line(-600,-200,600,-200)
+    turtle.ht()
+    turtle.speed(0)
+    turtle.pencolor("aqua")
+    turtle.bgcolor("black")
+    line(-600,-300,600,-300)
     time.sleep(10)
-    tur.clear()
+    turtle.reset()
