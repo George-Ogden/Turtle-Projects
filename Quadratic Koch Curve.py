ffad1e2f479f5d5fb1e48c30260ef097510b546b
George-Ogden <38294960+George-Ogden@users.noreply.github.com>
2019-04-01 14:07:52 +0100

    New Fractal!

---
+++ Quadratic Koch Curve.py
@@ -0,0 +1,37 @@
+import time
+import math
+import turtle
+def line(startx,starty, endx,endy):
+    fullx = endx-startx
+    fully = endy-starty
+    distance = math.sqrt(fully**2+fullx**2)
+    if distance < 6:
+        turtle.penup()
+        turtle.goto(startx,starty)
+        turtle.pendown()
+        turtle.goto(endx,endy)
+        return
+    thirdx = fullx/3
+    thirdy = fully/3
+    Ax = startx+thirdx
+    Ay = starty+thirdy
+    Dx = endx-thirdx
+    Dy = endy-thirdy
+    Bx = Ax-thirdy
+    By = Ay+thirdx
+    Cx = Dx-thirdy
+    Cy = Dy+thirdx
+    line(startx,starty, Ax,Ay)
+    line(Ax,Ay, Bx,By)
+    line(Bx,By, Cx,Cy)
+    line(Cx,Cy, Dx,Dy)
+    line(Dx,Dy, endx,endy)
+
+while True:
+    turtle.ht()
+    turtle.speed(0)
+    turtle.pencolor("aqua")
+    turtle.bgcolor("black")
+    line(-600,-300,600,-300)
+    time.sleep(10)
+    turtle.reset()
